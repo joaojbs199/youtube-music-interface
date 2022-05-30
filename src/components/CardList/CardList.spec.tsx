f0689796f3b4d6bb27fbc87fc98eac3174039870
João Batista da Silva <joaojbs199@gmail.com>
2022-05-30 00:58:17 -0300

    Centralização das importações de componentes e dados para renderizar. Teste de source de imagem dos cards.

--- src/components/CardList/CardList.spec.tsx
+++ src/components/CardList/CardList.spec.tsx
@@ -9,4 +9,14 @@ describe("Cards list test", () => {
         const cardList = container.getElementsByClassName("cardContainer").length;
         expect(cardList).toBe(cards.length - 1);
     });
+
+    it("Should test image source of a card", () => {
+        const { container } = render(<CardList cards={cards} />);
+        const cardList = container.getElementsByClassName("cardList")[0].children;
+        
+        for(let i = 1; i < cardList.length; i++) {
+            const src = cardList[i].children[0].getAttribute("src");
+            expect(src).toBe(cards[i + 1].img)
+        }
+    });
 });
